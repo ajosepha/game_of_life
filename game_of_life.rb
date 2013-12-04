1129605cb9300a8951191feb76cbf3d0dbc7fbf4
ajosepha <ariel.isaacs@gmail.com>
2013-12-03 19:37:40 -0500

    add live code 31min

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,25 +1,54 @@
+
 class Cell
-    #two states, nil and alive (represented by an x)
-    def initialize
-    end
+  attr_accessor :world, :x, :y 
+#     #two states, nil and alive (represented by an x)
+#     # def initialize#(board, x, y)
+#     #   #board = World.new
+#     # end
 
-    def neighbors
-         []
-    end
+  def initialize(world, x=0, y=0)
+    @world = world
+    @x = x
+    @y = y
+    world.cells << self
+  end
 
+  def neighbors
+    @neighbors = []
+     world.cells.each do |cell|  
+      if self.x == cell.x && cell 
+    end  
+    @neighbors
+        #will have 8 if statements iterating thorugh the array
+        #this will ideally count 
+      #the neighbors and return a count
+  end
+
+  def spawn_at(x, y)
+    Cell.new(world, x, y)
+  end
 end
 
 
-def Board
+class World
 
-
-    # attr_accessor :board
+  attr_accessor :cells
 
     def initialize
-       @board = board
+        #@dimensions = dimensions
+        @cells = []
     end
 
-     def new_board(dimensions)
-      @board = Array.new(dimensions) {Array.new(dimensions)}
-    end
+    #  def new_board
+    #   @dimensions.times do |x|
+    #     #x is the big array
+    #     #there are dimensions rows 
+    #     #the value of x and y will be equal to each other
+    #     @board << []
+    #         @dimensions.times do |y|
+    #             #y is the smaller arrays and slots
+    #             @board[x] << Cell.new(x, y)
+    #         end
+    #   end
+    # end
 end
--- game_of_life_spec.rb
+++ game_of_life_spec.rb
@@ -3,16 +3,39 @@ require './game_of_life.rb'
 
 
 describe "game of life" do
-	it "rule 1: any live cell with fewer than three live neigbors dies" do
+    let(:world) {World.new}
+    context "cell utility methods" do
+        subject { Cell.new(world) }
+        it "spawn relative to" do
+            cell = subject.spawn_at(3,5)
+            cell.is_a?(Cell).should be_true
+            cell.x.should == 3
+            cell.y.should == 5
+            cell.world.should == subject.world
+        end
+    end
+
+    it "detects a neigbor to the north" do
+        cell = subject.spawn_at(0, 1)
+       subject.neighbors.count.should == 1
+    end
+	
+    it "rule 1: any live cell with fewer than two live neigbors dies" do
     cell = Cell.new
     cell.neighbors.count.should == 0
     end
 end
 
-describe "Board" do
-    it "can make a board" do
-    board = Board.new
-    expect(board).to_not be(nil)
-    end
+# describe "World" do
+#     it "can make a board" do
+#         my_board = World.new(2)
+#         expect(my_board).to_not be(nil)
+#     end
+    # it "creates an array with with a cell in each slot" do
+    #     my_world = World.new(3)
+    #     my_world.new_board.should == [[ , , ],[ , , ,],[ , , ]]
+    # end
+#end 
 
-end
+#simple tests
+#
